b763d5fa5db1ff8044ec263fd4e487ddda5b3d3f
tbvjchvkfl <anfmdehdnjs@gmail.com>
2023-07-09 15:36:48 +0900

    초기 커밋.

---
+++ Main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+
+int Sum(int x, int y)
+{
+	return x + y;
+}
+
+int main()
+{
+	int array[2][3];
+
+	int(*p)[3] = &array[0];
+
+
+	//std::cout << Sum(1, 2) << std::endl;
+}
+
+
+
+
+
+
